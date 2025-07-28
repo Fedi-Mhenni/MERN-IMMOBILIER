ebe7d94f31e93f9b53b66e16dc490f2f87a6c6cb
Fedi <fedimhenni19@gmail.com>
2025-07-28 15:53:48 +0200

    Compléter les fonctionnalités de la page de connexion (sign in)

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,8 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
 import jwt from 'jsonwebtoken';
+import { errorHandler } from '../utils/error.js';
+
 
 export const signup = async(req, res,next) => {
     const{ username, email, password } = req.body;
@@ -18,9 +20,9 @@ export const signin = async(req, res,next) => {
     const { email, password } = req.body;
     try{
         const validuser = await User.findOne({ email });
-        if(!validuser) return next(errorhandler(404, "User not found"));
+        if(!validuser) return next(errorHandler(404, "User not found"));
         const validpassword =  bcryptjs.compareSync(password, validuser.password);
-        if(!validpassword) return next(errorhandler(401, "Wrong credential !"));
+        if(!validpassword) return next(errorHandler(401, "Wrong credential !"));
         const token = jwt.sign({ id: validuser._id }, process.env.JWT_SECRET);
         const{ password: pass, ...rest} = validuser._doc;
         res
