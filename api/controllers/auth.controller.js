6435a75e2593ece209eca5dfc315eab46f956ad9
Fedi <fedimhenni19@gmail.com>
2025-07-28 15:32:33 +0200

    Implémentation de la route API de connexion utilisateur (/signin)

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 export const signup = async(req, res,next) => {
     const{ username, email, password } = req.body;
@@ -12,3 +13,22 @@ export const signup = async(req, res,next) => {
         next(err);
     }  
 };
+
+export const signin = async(req, res,next) => {
+    const { email, password } = req.body;
+    try{
+        const validuser = await User.findOne({ email });
+        if(!validuser) return next(errorhandler(404, "User not found"));
+        const validpassword =  bcryptjs.compareSync(password, validuser.password);
+        if(!validpassword) return next(errorhandler(401, "Wrong credential !"));
+        const token = jwt.sign({ id: validuser._id }, process.env.JWT_SECRET);
+        const{ password: pass, ...rest} = validuser._doc;
+        res
+        .cookie('acess_token',token,{httpOnly : true})
+        .status(200)
+        .json(rest);
+    }
+    catch(err){
+        next(err);
+    }  
+}
